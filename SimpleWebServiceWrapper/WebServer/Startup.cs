1f87c0bf42f184b97bd765b9e0d82896c9cf2a29
Gustavo Van Overberghe <gusvano@gmail.com>
2020-03-29 15:45:16 -0400

    Initial Web Server Commit
    Adds initial Kestrel Web Service starting with Windows Service.

---
+++ SimpleWebServiceWrapper/WebServer/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rmWebServiceWrapper.WebServer
+{
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
+      public IConfiguration Configuration { get; }
+
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.Configure<KestrelServerOptions>(
+            Configuration.GetSection("Kestrel"));
+      }
+
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         
+      }
+   }
+}
